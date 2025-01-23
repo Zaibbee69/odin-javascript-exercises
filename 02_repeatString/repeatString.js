b33c32cadb93aa3eb247eb1ee3f177ba3b719b4b
Zaibbee69 <jahanzaib12569@gmail.com>
2025-01-23 22:07:25 +0500

    Second Exercse done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numberOfTimes) {
 
+    // Making a check if random times is selected
+    if (numberOfTimes < 0) {
+        return "ERROR";
+    } 
+
+    // Making a value to hold the data
+    let repeatedString = "";
+
+    // Looping over the required times
+    for (let index = 0; index < numberOfTimes; index++) {
+        repeatedString += stringToRepeat; 
+    }
+
+    // Returning the string
+    return repeatedString;
 };
 
 // Do not edit below this line
