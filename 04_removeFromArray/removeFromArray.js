a232d3f61e5e525234dde608bb41f7ed5366c15c
Zaibbee69 <jahanzaib12569@gmail.com>
2025-01-24 14:42:10 +0500

    Made the basic functionality of the program

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayFromRemoving, elementToBeRemoved) {
+    let arrayLength = arrayFromRemoving.length;
+
+    for (let index = 0; index < arrayLength; index++) {
+        if (arrayFromRemoving[index] == elementToBeRemoved)
+        {
+            arrayFromRemoving.splice(index, 1);
+        }  
+    }
+    return arrayFromRemoving;
 };
 
 // Do not edit below this line
