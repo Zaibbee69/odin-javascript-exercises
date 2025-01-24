9cb3d55a4986cf3dba645d1792efd2f8efd3c5a6
Zaibbee69 <jahanzaib12569@gmail.com>
2025-01-24 23:37:30 +0500

    Exercise 4 Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,16 @@
-const removeFromArray = function(arrayFromRemoving, elementToBeRemoved) {
-    let arrayLength = arrayFromRemoving.length;
+const removeFromArray = function(arrayFromRemoving, ...elementsToBeRemoved) {
 
-    for (let index = 0; index < arrayLength; index++) {
-        if (arrayFromRemoving[index] == elementToBeRemoved)
-        {
+    // Looping over the args one by one removing required index
+    elementsToBeRemoved.forEach(element => {
+        
+        // Keep removing the element until it no longer exists in the array
+        let index = arrayFromRemoving.indexOf(element);
+        while (index > -1) {
             arrayFromRemoving.splice(index, 1);
-        }  
-    }
+            index = arrayFromRemoving.indexOf(element);
+        }
+    });
+    
     return arrayFromRemoving;
 };
 
