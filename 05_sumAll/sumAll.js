7516e4b5fcc2e013c86d21fe17d52ba6c46ea1d8
Zaibbee69 <jahanzaib12569@gmail.com>
2025-01-25 15:27:21 +0500

    Exercise Five Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    // Checking if value is negative or non integer type
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0)
+    {
+        return "ERROR";
+    }
+
+    // Variable to hold the final sum and largest value
+    let finalSum = 0;
+    let largestNum = 0;
+    let smallestNum = 0;
+
+    // Finding out which value is greater first
+    if (num1 > num2) {
+        largestNum = num1;
+        smallestNum = num2;
+    }
+    else 
+    {
+        largestNum = num2;
+        smallestNum = num1;
+    }
+
+    for ( let i = 0; i < largestNum; i ++ )   
+    {
+        finalSum += smallestNum + i;
+    }
+
+    // Returning data
+    return finalSum;
 
 };
 
