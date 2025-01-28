fc1a7db9504a4f2a7859408c01381c6a287e6750
Zaibbee69 <jahanzaib12569@gmail.com>
2025-01-29 00:51:18 +0500

    Ninth Exercise Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    // Ok first making a standard string
+    const standardStr = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    // Now adding thing which are in the standard
+    const cleanedStr = str
+    .toLowerCase()
+    .split("")
+    .filter(character => standardStr.includes(character))
+    .join("");
+
+    // Create a new reversed string
+    const reversedStr = cleanedStr.split("").reverse().join("");
+
+    return cleanedStr === reversedStr;
+
+
 
 };
 
