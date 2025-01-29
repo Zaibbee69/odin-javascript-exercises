4a550d9c103ba98fc48fc371bece526f2ba7177c
Zaibbee69 <jahanzaib12569@gmail.com>
2025-01-29 16:31:58 +0500

    Tenth Exercise Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    // Also parsing the data if it's a string
+    Number.parseInt(num);
+
+    // Making data checks
+    if (num < 0) return "OOPS"; 
+    if (num == 0) return 0;
+
+    // Making variables for holding data
+    let prev = 0;
+    let current = 1;
+    let next = 1;
+
+    // Finding data upto term
+    for( let i = 1; i < num; i ++ )
+    {
+        next = prev + current;
+        prev = current;
+        current = next;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
