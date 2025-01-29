f8054b8052ec7d0b4926e43e8ea06a30fc56996d
Zaibbee69 <jahanzaib12569@gmail.com>
2025-01-29 20:36:14 +0500

    Last Exercise Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(peoples) {
+    const currentYear = new Date().getFullYear();
 
+    return peoples.reduce((oldest, person) => {
+        const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+        return personAge > oldestAge ? person : oldest;
+    });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
